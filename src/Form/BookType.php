1ca521f12b6df729b531c3560136a0002b965ad5
Gregoire-F <gregoire.frou@gmail.com>
2026-04-22 17:56:42 +0200

    FIX CRUD et login

---
+++ src/Form/BookType.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Author;
+use App\Entity\Book;
+use App\Entity\category;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class BookType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('title')
+            ->add('year')
+            ->add('description')
+            ->add('Author', EntityType::class, [
+                'class' => Author::class,
+                'choice_label' => 'id',
+            ])
+            ->add('category', EntityType::class, [
+                'class' => category::class,
+                'choice_label' => 'id',
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Book::class,
+        ]);
+    }
+}
