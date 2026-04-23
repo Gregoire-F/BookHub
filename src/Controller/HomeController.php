f0bc4eb8bf2000053bfe4f36292296f2c171ed27
Jules-95 <jlsmillet@yahoo.fr>
2026-04-23 12:36:49 +0200

    add : Page d'accueil

---
+++ src/Controller/HomeController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+final class HomeController extends AbstractController
+{
+    #[Route('/', name: 'app_home')]
+    public function index(): Response
+    {
+        return $this->render('home/index.html.twig');
+    }
+
+    #[Route('/book', name: 'app_book')]
+    public function book(): Response
+    {
+        return $this->render('book/index.html.twig');
+    }
+    
+}
